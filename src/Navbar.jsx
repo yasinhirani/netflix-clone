2f3cca8eeec5072db8875d0a330a9c67c8484ddc
Yasin Hirani <hianiyasin384@gmail.com>
2022-06-09 11:13:37 +0530

    Fix : Removed unused comments

--- src/Navbar.jsx
+++ src/Navbar.jsx
@@ -16,35 +16,9 @@ const Navbar = () => {
           <figure className='w-8 rounded-full overflow-hidden'>
             <img src={user.picture} alt="" />
           </figure>
-          <h4 className='text-white'>{user.nickname}</h4>
+          <h4 className='text-white hidden md:block'>{user.nickname}</h4>
         {isAuthenticated && <button className='text-white' onClick={() => logout()}>Logout</button>}
         </div>}
-        {/* <div className="flex items-center space-x-4">
-          <input
-            type="text"
-            className="outline-none focus:ring-0 px-2 py-1 rounded-md border border-gray-400"
-            name="movie_name"
-            id="movie_name"
-            placeholder="Search..."
-            autoComplete="off"
-          />
-          <button className="text-white">
-            <svg
-              xmlns="http://www.w3.org/2000/svg"
-              className="w-5"
-              fill="none"
-              viewBox="0 0 24 24"
-              stroke="currentColor"
-              stroke-width="2"
-            >
-              <path
-                stroke-linecap="round"
-                stroke-linejoin="round"
-                d="M21 21l-6-6m2-5a7 7 0 11-14 0 7 7 0 0114 0z"
-              />
-            </svg>
-          </button>
-        </div> */}
       </nav>
     )
 }
